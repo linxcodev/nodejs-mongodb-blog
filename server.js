68943c4f96a0505fef1a83faa1da387d4920bd41
linxcodev <fajrul452@gmail.com>
2019-11-25 20:13:54 +0700

    post blog

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert')
+const bodyParser = require('body-parser');
 
 app.use("/static", express.static("static"))
 app.set("view engine", "ejs")
+
+// conf bodyParser
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 // Connection URL
 const url = 'mongodb://localhost:27017'
@@ -16,7 +21,7 @@ let client = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: tr
 // Use connect method to connect to the server
 client.connect(err => {
   assert.equal(null, err);
-  console.log("Connected successfully to server")
+  console.log("Connected successfully to dbserver")
 
   const db = client.db(dbName);
 
@@ -28,9 +33,19 @@ client.connect(err => {
     res.render("admin/dashboard")
   })
 
+  app.get('/admin/posts', (req, res) => {
+    res.render("admin/posts")
+  })
+
+  app.post('/do-post', (req, res) => {
+    db.collection("posts").insertOne(req.body, (err, doc) => {
+      res.send("posted successfully")
+    })
+  })
+
   client.close()
 })
 
 app.listen(3100, () => {
-  console.log("server run on port 310");
+  console.log("server run on port 3100");
 })
